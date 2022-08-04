683af7bc2570ff99d99aa8069e990feb3520fc4c
mdislam045 <83195905+mdislam045@users.noreply.github.com>
2022-08-03 22:54:16 -0400

    added for learning purpose

--- src/main/java/com/example/demo/HelloworldApplication.java
+++ src/main/java/com/example/demo/HelloworldApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloworldApplication {
-
+// this is a demo for git
+// this is a new demo
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
